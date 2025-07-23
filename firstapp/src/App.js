39bcdcab14e682642d723c9f51a152466dde1b6b
Nency Vadadoriya <vadadoriyanency8@gmail.com>
2025-07-23 19:43:02 +0530

    My First React App

---
+++ firstapp/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>name : Nency Vadadoriya</h1>
+      <p>GRID : 6574</p>
+      <p>phone : 9023582238</p>
+      <p>course : full stack devlopment</p>
+      <img src="https://media.istockphoto.com/id/1550071750/photo/green-tea-tree-leaves-camellia-sinensis-in-organic-farm-sunlight-fresh-young-tender-bud.jpg?s=612x612&w=0&k=20&c=RC_xD5DY5qPH_hpqeOY1g1pM6bJgGJSssWYjVIvvoLw="/>
+    </div>
+  );
+}
+
+export default App;
---
+++ firstreactapp/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <h1>name : nency vadadoriya</h1>
+      <p>age : 19</p>
+      <p>city : surat</p>
+      <p>course : full stack devlopment</p>
+      <p>phone : 9023582238</p>
+    </>
+  )
+}
+
+export default App
