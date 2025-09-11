8478464541042a4f3bf2bca030dd7085ce43bdc5
Nency Vadadoriya <vadadoriyanency8@gmail.com>
2025-09-11 15:54:15 +0530

    bg-changer

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -1,27 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [color, setColor] = useState("white");
-  const colorList = ["green", "red", "yellow", "purple", "blue", "cyan", "teal"]
+  const [theme, setTheme] = useState("light");
+
+  const colorList = ["green", "red", "yellow", "purple", "blue", "cyan", "teal"];
 
   return (
     <div className="w-full h-screen transition-colors duration-700" style={{ backgroundColor: color }}>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      {/* Navbar */}
+      <nav className="bg-black dark:bg-gray-900 border-gray-200">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="#"
-            className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://icon-library.com/images/change-icon-png/change-icon-png-19.jpg" className="h-8" alt="Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Background Change</span>
+
+          {/* Logo/Title */}
+          <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+              Background Changer
+            </span>
           </a>
 
-          <div className="flex gap-2 flex-wrap">
-            {colorList.map((value, index) =>
-              <button key={index} onClick={() => setColor(value)} className={`px-4 py-2 text-white bg-${value}-500 hover:bg-${value}-600 rounded`}>{value[0].toUpperCase() + value.slice(1, value.length)}</button>
-            )}
+          {/* Buttons */}
+          <div className="flex gap-2 flex-wrap items-center">
+            {/* Color buttons */}
+            {colorList.map((value, index) => (
+              <button
+                key={index}
+                onClick={() => setColor(value)}
+                className={`px-4 py-2 text-white rounded transition duration-300  bg-${value}-500 hover:bg-${value}-600`}>{value[0].toUpperCase() + value.slice(1)}
+              </button>
+            ))}
           </div>
         </div>
-      </nav >
-    </div >
+      </nav>
+    </div>
   );
 }
 
